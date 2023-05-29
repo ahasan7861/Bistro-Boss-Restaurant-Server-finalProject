43c261428b3e420bcdd0103fe43dc93599307d29
ahasan7861 <ahfahasan786@gmail.com>
2023-05-29 11:37:06 +0600

    delete items

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -47,12 +47,30 @@ async function run() {
     })
 
 
-    //cart collection
+    //cart collection api's
+
+    app.get('/carts', async(req, res) =>{
+      const email = req.query.email;
+      console.log(email)
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
 
     app.post('/carts', async(req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
