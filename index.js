5420ed3ed930375fefa580bf3980a4b6bf2e22e8
ahasan7861 <ahfahasan786@gmail.com>
2023-05-30 14:27:53 +0600

    UPDATEEED

--- index.js
+++ index.js
@@ -32,15 +32,41 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+    //users related api's
 
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async(req, res) => {
+        const user = req.body;
+        
+        const query = {email: user.email}
+        const existingUser = await usersCollection.findOne(query);
+        
+        if(existingUser){
+          return res.send({message: 'user already exists'})
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    })
+
+
+
+
+    //menu related api's
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result);
     })
+
+    //review related api's
     app.get('/reviews', async(req, res) => {
         const result = await reviewCollection.find().toArray();
         res.send(result);
@@ -48,6 +74,8 @@ async function run() {
 
 
     //cart collection api's
+
+    
 
     app.get('/carts', async(req, res) =>{
       const email = req.query.email;
